1146293ce43ee0864bffcb88a66d2f8c3a0de45d
Robert Greiner <robert@robertgreiner.com>
2011-08-10 08:10:13 -0500

    Refactored NumberText to use a Dictionary<int, string) instead of an integer array.

--- NumberText/NumberText.cs
+++ NumberText/NumberText.cs
@@ -6,22 +6,28 @@ using System.Text;
 namespace NumberText {
     public class NumberText {
 
-        private string[] textStrings = {
-                                           "zero", 
-                                           "one", 
-                                           "two", 
-                                           "three", 
-                                           "four", 
-                                           "five", 
-                                           "six", 
-                                           "seven", 
-                                           "eight", 
-                                           "nine"
-                                       };
+        private Dictionary<int, string> textStrings = new Dictionary<int, string>();
+
+        public NumberText() {
+            Initialize();
+        }
 
         //TODO make this an extension method for the Integer object?
         public string ToText(int num) {
             return textStrings[num];
+        }
+
+        private void Initialize() {
+            textStrings.Add(0, "zero");
+            textStrings.Add(1, "one");
+            textStrings.Add(2, "two");
+            textStrings.Add(3, "three");
+            textStrings.Add(4, "four");
+            textStrings.Add(5, "five");
+            textStrings.Add(6, "six");
+            textStrings.Add(7, "seven");
+            textStrings.Add(8, "eight");
+            textStrings.Add(9, "nine");
         }
     }
 }
