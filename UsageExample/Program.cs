88eda7c5dcbecc4397fc6f6ff3e910ee0bb4eddd
Robert Greiner <robert@robertgreiner.com>
2011-08-19 14:57:48 -0500

    Added usage example for integer.ToText()

---
+++ UsageExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NumberText;
+
+namespace UsageExample {
+    class Program {
+        static void Main(string[] args) {
+            int testNumber = 1234567;
+            Console.WriteLine(testNumber.ToText());
+            Console.WriteLine(98765.ToText());
+            Console.ReadKey();
+        }
+    }
+}
